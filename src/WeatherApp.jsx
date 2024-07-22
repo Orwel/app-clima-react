266f7d5cad41ae2025bb07096632df6ee4bc1ba7
Orwel <jdvanegas@hotmail.com>
2024-07-22 11:48:55 -0500

    Realizamos una app de clima usando una API y CSS y React

---
+++ src/WeatherApp.jsx
@@ -0,0 +1,65 @@
+import './WeatherApp.css'
+import { useState } from 'react'
+export const WeatherApp = () => {
+
+    const [city, setCity] = useState('')
+    const [weatherData, setWeatherData] = useState(null)
+
+    const urlBase = 'https://api.openweathermap.org/data/2.5/weather'
+    const API_KEY = '605507acf87117e111e54a3ab5238541'
+    const difKelvin = 275.15
+
+    const fetchWheaterData = async () => {
+        try {
+            const response = await fetch(`${urlBase}?q=${city}&appid=${API_KEY}&lang=es`)
+            const data = await response.json()
+            setWeatherData(data)
+            console.log(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+    const handleCityChange = (e) => {
+        setCity(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        fetchWheaterData()
+        console.log(city)
+    }
+
+    return (
+        <div className="container">
+            <h1>Aplicación clima</h1>
+            <form onSubmit={handleSubmit}>
+                <input 
+                type="text" 
+                placeholder="Ingresa una ciudad"
+                value={city}
+                onChange={handleCityChange}
+                >
+
+                </input>
+                <button 
+                type="submit"
+                >
+                    Buscar</button>
+                    </form>
+
+{weatherData && (
+    <div className="weather-data">
+        <h2>{weatherData.name}, {weatherData.sys.country} {weatherData.coord.lat}</h2>
+        <p>La temperatura actual es: {Math.floor(weatherData.main.temp - difKelvin)} ºC {weatherData.sys.id} </p>
+        <p>Temperatura actual: {(weatherData.main.temp - difKelvin).toFixed(2)}°C</p>
+        <p>La condición metereológica actual: {weatherData.weather[0].description}</p>
+        <img 
+        src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`} 
+        alt="weather icon"/>
+    </div>
+)}
+</div>
+);
+}
